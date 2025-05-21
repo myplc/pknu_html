353aab16f6b71fcd3416ca14d0aa221a51c7df3f
myplc <plctohmi@gmail.com>
2025-05-21 12:00:40 +0900

    1

---
+++ ex18-01b.js
@@ -0,0 +1,4 @@
+function fn (...arr){ // 나머지 매개변수
+    console.log(...arr) // 전개연산자
+}
+fn(1,2,3,4,5,6)
---
+++ ex18-01d.js
@@ -0,0 +1,20 @@
+/* 객체 다루기 */
+const user = {name:"홍길동", age:10}
+// 객체의 키로 값을 읽는법 R
+console.log(user.name)
+console.log(user['name'])
+// 객체를 추가하는 법 C
+user.gender = "male"
+user['address'] = "부산"
+// 수정하는법 U
+user.age = 17
+user['name'] = '슈퍼맨'
+// 삭제하는 법 D
+delete user['gender']
+// 출력하기
+console.log(user)
+
+// 객체 // Tip
+// const test = {test}  // {test:test}
+const updated = { ...user , age:20} // 외워라!!!!
+console.log(updated)
